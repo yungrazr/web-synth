824b0d26d85adc62f338011a4f25472f296a9c03
Daniel Vegera <daniel.vegera@gmail.com>
2021-02-11 19:55:53 +0000

    Synth working polyphony, 3xosc, filter & delay

--- src/App.js
+++ src/App.js
@@ -1,25 +1,120 @@
-import logo from './logo.svg';
+import React, { useState} from 'react';
 import './App.css';
+import Audio from './components/Audio'
+import Oscillator from './components/Oscillator'
+import Delay from './components/Delay'
+import Filter from './components/Filter'
+import { Knob } from "react-rotary-knob";
+import { Piano, KeyboardShortcuts, MidiNumbers } from 'react-piano';
+import 'react-piano/dist/styles.css';
+import './CustomPianoStyles.css';  // import a set of overrides
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [start, setStart] = useState(false)
+    const firstNote = MidiNumbers.fromNote('c4');
+    const lastNote = MidiNumbers.fromNote('c5');
+    const keyboardShortcuts = KeyboardShortcuts.create({
+        firstNote: firstNote,
+        lastNote: lastNote,
+        keyboardConfig: KeyboardShortcuts.HOME_ROW,
+    });
+
+    // [midiNumber, frequency]
+    const noteFrequencies = new Map([
+        [60, 262], [61, 277.2], [62, 293.7],
+        [63, 311.2], [64, 329.7], [65, 349.3],
+        [66, 370.1], [67, 392.1], [68, 415.4],
+        [69, 440], [70, 466.1], [71, 493.8],
+        [72, 523.2]
+    ]);
+
+    // set state to represent initial value of masterGainNode
+    const [masterGainValue, setMasterGainValue] = useState(0.5)
+    const [playNote, setPlayNote] = useState({ playNote: null, stopNote: null })
+
+    const initializeMasterGain = () => {
+        setStart(true)
+        // Connect the masterGainNode to the audio context to allow it to output sound.
+        Audio.masterGainNode.connect(Audio.context.destination)
+
+        // Set masterGain Value to 0
+        Audio.masterGainNode.gain.setValueAtTime(0.2, Audio.context.currentTime)
+    }
+
+    // // initialize masterGainNode on first render
+    // useEffect(() => {
+    //     initializeMasterGain()
+    // }, [])
+
+    function PianoRoll() {
+        return (
+        <div className="piano-container">
+            <Piano
+                noteRange={{ first: firstNote, last: lastNote }}
+                playNote={(midiNumber) => {
+                    onKeyDown(midiNumber);
+                }}
+                stopNote={(midiNumber) => {
+                    setPlayNote({ stopNote: midiNumber, playNote: null })
+                }}
+                keyboardShortcuts={keyboardShortcuts}
+            />
+        </div>)
+    }
+
+    const changeMasterVolume = (e) => {
+        setMasterGainValue(Math.round(e) / 100)
+        Audio.masterGainNode.gain.setValueAtTime(Math.round(e) / 100, Audio.context.currentTime)
+    }
+
+    function onKeyDown(midiNumber) {
+        setPlayNote({ playNote: midiNumber, frequency: noteFrequencies.get(midiNumber), stopNote: null })
+    }
+
+    function MasterVolume() {
+        return (
+            <div className="master-volume-container">
+            <h1 className="oscillator-title">MASTER VOL</h1>
+            <Knob
+                onChange={changeMasterVolume}
+                unlockDistance={10}
+                preciseMode={true}
+                min={0}
+                max={100}
+                rotateDegrees={225}
+                value={masterGainValue * 100} />
+            <p>{Math.round(masterGainValue * 100)}</p>
+        </div>
+        )
+    }
+
+
+    if(start===false)
+    {
+        return (        
+        <div className="App">
+            <div className="splash-screen">
+                <h1>Moms.On.My.Synth</h1>
+                <p onClick={()=>initializeMasterGain()}>Click here to start</p>
+            </div>
+        </div>);
+    }
+    else {
+        return (
+            <div className="App">
+                <div className="top-row">
+                    <Oscillator oscNum={1} play={playNote} />
+                    <Oscillator oscNum={2} play={playNote} />
+                    <Oscillator oscNum={3} play={playNote} />
+                    <Filter />
+                    <Delay />
+                    {MasterVolume()}
+                </div>
+                <div className="bottom-row">
+                    {PianoRoll()}
+                </div>
+            </div>
+        );
+    }
 }
-
 export default App;
---
+++ src/components/Delay.js
@@ -0,0 +1,140 @@
+import React, { useState, useEffect } from 'react';
+import './Oscillator.css';
+import Audio from './Audio'
+import { Knob } from "react-rotary-knob";
+
+const Delay = () => {
+    const [delay, setDelay] = useState();
+    const [feedback, setFeedback] = useState();
+    const [filter, setFilter] = useState();
+    const [offOn, setOffOn] = useState(false)
+
+    // initialize masterGainNode on first render
+    useEffect(() => {
+        const initiateDelay = () => {
+            let delayNode = Audio.context.createDelay();
+            delayNode.delayTime.setValueAtTime(0.15, Audio.context.currentTime)
+    
+            let feedbackNode = Audio.context.createGain();
+            feedbackNode.gain.setValueAtTime(0.6, Audio.context.currentTime)
+    
+            let filterNode = Audio.context.createBiquadFilter();
+            filterNode.frequency.setValueAtTime(1000, Audio.context.currentTime)
+    
+            delayNode.connect(feedbackNode);
+            feedbackNode.connect(filterNode);
+            filterNode.connect(delayNode);
+    
+            Audio.masterGainNode.connect(delayNode);
+    
+            const delayNodeValues = {
+                delayTime: 0.15,
+                delayNode: delayNode,
+            }
+            const filterNodeValues = {
+                frequency: 1000,
+                filterNode: filterNode,
+            }
+            const feedbackNodeValues = {
+                gain: 0.60,
+                feedbackNode: feedbackNode,
+            }
+    
+            setDelay(delayNodeValues);
+            setFeedback(feedbackNodeValues)
+            setFilter(filterNodeValues)
+        }
+        initiateDelay()
+    }, [])
+
+    const updateDelayTime = (e) => {
+        let newDelay = {...delay};
+        newDelay.delayNode.delayTime.setValueAtTime(e, Audio.context.currentTime)
+        newDelay.delayTime = e;
+        //newDelay.delayTime.value = e;
+        setDelay(newDelay)
+    }
+
+    const updateDelayFeedback = (e) => {
+        let newFeedback = {...feedback};
+        newFeedback.gain = e;
+        newFeedback.feedbackNode.gain.setValueAtTime(e, Audio.context.currentTime)
+        setFeedback(newFeedback)
+    }
+
+    const updateDelayCutoffFreq = (e) => {
+        let newFilter = {...filter};
+        newFilter.frequency = e;
+        newFilter.filterNode.frequency.setValueAtTime(e, Audio.context.currentTime)
+        setFilter(newFilter)
+    }
+
+    const switchOnOffDelay=()=>{
+        if(offOn) {
+            setOffOn(false)
+            delay.delayNode.disconnect(Audio.context.destination);
+        }
+        else {
+            setOffOn(true)
+            delay.delayNode.connect(Audio.context.destination);
+        }
+    }
+
+    if(delay && filter && feedback) {
+        return (
+            <div className="oscillator-container">
+                <h1 className="oscillator-title">DELAY</h1>
+    
+                {/* Set the value of .frequency element to be the frequency of the selected oscillator
+                and add onChange handler to change the frequency of selectedOscillatorNode */}
+                <div className="oscillator-tune-container">
+                    <h3>TIME</h3>
+                    <Knob
+                        onChange={updateDelayTime}
+                        unlockDistance={10}
+                        min={0}
+                        max={1}
+                        preciseMode={true}
+                        rotateDegrees={180}
+                        value={delay.delayTime} />
+                    <p>{(delay.delayTime).toFixed(2)}sec</p>
+                </div>
+    
+                {/* Set the value of .wave-type element to be the type of the selected oscillator
+                and add onChange handler to change the type of selectedOscillatorNode */}
+                <div className="oscillator-tune-container">
+                    <h3>FEEDBACK</h3>
+                    <Knob
+                        onChange={updateDelayFeedback}
+                        unlockDistance={10}
+                        min={0}
+                        max={0.95}
+                        preciseMode={true}
+                        rotateDegrees={180}
+                        value={feedback.gain} />
+                    <p>{feedback.gain.toFixed(2)}</p>
+                </div>
+    
+                <div className="oscillator-vol-container">
+                    <h3>CUTOFF</h3>
+                    {/* Set the value of .oscillator-volume element to be the gain value of the selected oscillator's GainNode
+                    and add onChange handler to change the gain of selectedOscillatorNode */}
+                    <Knob
+                        onChange={updateDelayCutoffFreq}
+                        unlockDistance={10}
+                        preciseMode={true}
+                        min={0}
+                        max={4000}
+                        rotateDegrees={180}
+                        value={filter.frequency} />
+                    <p>{Math.round(filter.frequency)}hz</p>
+                </div>
+                <h3 onClick={switchOnOffDelay}>{offOn? "Turn Off" : "Turn On"}</h3>
+            </div>
+        );
+    } else { 
+        return null
+    }
+
+}
+export default Delay;
